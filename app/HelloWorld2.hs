73a49564ee00452b95637eb035c6038338c915ee
Koharu Saeki <koharu0929alexandros@gmail.com>
2024-10-12 17:57:38 +0900

    non-HTML

--- app/HelloWorld2.hs
+++ app/HelloWorld2.hs
@@ -6,7 +6,9 @@ import           Yesod
 
 data Links = Links
 
+-- foundation
 mkYesod "Links" [parseRoutes|
+-- resource
 / HomeR GET
 /page1 Page1R GET
 /page2 Page2R GET
@@ -14,6 +16,7 @@ mkYesod "Links" [parseRoutes|
 
 instance Yesod Links
 
+-- handler function
 getHomeR  = defaultLayout [whamlet|<a href=@{Page1R}>Go to page 1!|]
 getPage1R = defaultLayout [whamlet|<a href=@{Page2R}>Go to page 2!|]
 getPage2R = defaultLayout [whamlet|<a href=@{HomeR}>Go home!|]
